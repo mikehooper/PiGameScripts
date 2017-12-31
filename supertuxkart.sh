55719501839b355163733fb9eb90716a977d2cf5
mikehooper <mike@onlineorienteer.co.uk>
2017-12-31 17:06:23 +0000

    Add files via upload

---
+++ supertuxkart.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# SuperTuxKart
+
+# https://github.com/supertuxkart/stk-code
+
+cd ~
+# install prerequisites
+sudo apt-get install build-essential cmake libbluetooth-dev libcurl4-gnutls-dev libfreetype6-dev libfribidi-dev libgl1-mesa-dev libjpeg-dev libogg-dev libopenal-dev libpng-dev libvorbis-dev libxrandr-dev mesa-common-dev pkg-config zlib1g-dev
+# get source
+git clone https://github.com/supertuxkart/stk-code.git stk-code
+# get assets
+svn co https://svn.code.sf.net/p/supertuxkart/code/stk-assets stk-assets
+
+cd ~/stk-code
+mkdir cmake_build
+cd cmake_build
+cmake .. -DUSE_GLES2=1 -DBUILD_RECORDER=off
+make -j4
